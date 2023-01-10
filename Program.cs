60179d6a98aadaf49e9140c465bb115ff752be5a
ShubhamGupta <92706349+ShubhamGupta2701@users.noreply.github.com>
2023-01-10 23:20:06 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 21;
+            bool male = true;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is Shubham");
+            Console.WriteLine("My age is : "+age);
+            Console.WriteLine("Are you male ? :  "+male); 
+            // Console.WriteLine(age);
+            // Console.Beep();  // Whenever my code compilation is complete it creates a sound.
+            Console.ReadKey();
+        }
+    }
+    
+} 
---
+++ Program2.cs
@@ -0,0 +1,9 @@
+using System;
+namespace Try_Code{
+    class Program2{
+        static void Main(){
+            Console.WriteLine("My name is Shubham and i am doing my Master's in Computer Application from Thapar Institute of Engineering and Technology");
+            Console.ReadKey();
+        }
+    }
+}
